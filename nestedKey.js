d453085f0b2292c05ab0236033e1895b2db8c9bb
Abhijit Gautam <abhijit.gautam1@wipro.com>
2021-02-23 21:12:39 +0000

    test

--- nestedKey.js
+++ nestedKey.js
@@ -1,13 +1,15 @@
-function getObjectKeys(object, key) {
-  const keys = key.split('.');
-  let obj = object;
-  for (let ikey of keys) {
-      for (let [objKey, value] of Object.entries(obj)) {
-          if(!keys.includes(objKey)) {
-              continue;
-          }
-          obj = value;
-      }
-  }
-  return obj;
+function getObjectKeys(o, k) {
+    const keys = k.split('.');
+    let obj = o;
+    for (let ikey of keys) {
+        for (let [objKey, value] of Object.entries(obj)) {
+            if(!keys.includes(objKey)) {
+                continue;
+            }
+            obj = value;
+        }
+    }
+    return obj;
 }
+
+module.exports = getObjectKeys;
