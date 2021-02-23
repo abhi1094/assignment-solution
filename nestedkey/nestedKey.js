1b619748f5106ba874b544da8b90dc42059b5ba7
Abhijit Gautam <abhijit.gautam1@wipro.com>
2021-02-23 21:30:20 +0000

    refactor

--- nestedkey/nestedKey.js
+++ nestedkey/nestedKey.js
@@ -1,13 +1,8 @@
 function getObjectKeys(o, k) {
     const keys = k.split('/');
     let obj = o;
-    for (let ikey of keys) {
-        for (let [objKey, value] of Object.entries(obj)) {
-            if(!keys.includes(objKey)) {
-                continue;
-            }
-            obj = value;
-        }
+    for(let i =0; i < keys.length ; i++) {
+        obj = obj[keys[i]]
     }
     return obj;
 }
